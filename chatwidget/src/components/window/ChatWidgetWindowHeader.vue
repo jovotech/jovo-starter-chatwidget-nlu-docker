7e355047e633456f193e0113ad17adf202d74f79
jankoenig <jankoenig120190@gmail.com>
2022-11-15 17:16:12 +0100

    💄 Add logo to header

--- chatwidget/src/components/window/ChatWidgetWindowHeader.vue
+++ chatwidget/src/components/window/ChatWidgetWindowHeader.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="p-6 bg-primary text-white flex items-center justify-between">
-    <h3 class="text-base font-bold">Jovo Sample Bot</h3>
+    <div class="flex items-center space-x-3">
+      <img class="h-10" src="@/assets/logo.svg" />
+      <h3 class="text-base font-bold">Jovo Sample Bot</h3>
+    </div>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       class="h-5 w-5 cursor-pointer hover:text-gray-100"
